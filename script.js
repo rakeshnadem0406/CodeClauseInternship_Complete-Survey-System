7ae67425a030b453208f460b64980848fce6b029
Rakeshnadem <126226811+rakeshnadem0406@users.noreply.github.com>
2023-11-30 22:59:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleChildrenInput(show) {
+    var childrenInput = document.getElementById("childrenInput");
+    childrenInput.style.display = show ? "block" : "none";
+  }
+  
+  function toggleDiseaseInput(show) {
+    var diseaseInput = document.getElementById("diseaseInput");
+    diseaseInput.style.display = show ? "block" : "none";
+  }
+  
